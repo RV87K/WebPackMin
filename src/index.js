63f00100dc98d88720f4e37ccc8f9f9a28d58f80
Roman K <39156575+RV87K@users.noreply.github.com>
2022-11-24 21:32:29 +1000

    initial settings untill plug analytics and use pattern [name]

--- src/index.js
+++ src/index.js
@@ -1,2 +1,3 @@
+import Post from "./post";
 const post = new Post('WebPack Post Title')
 console.log('Post to String:', post.toString())
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path')
 
 module.exports = {
-    entry: './src/index.js',  //указываем входной файл
+    mode: 'development',
+    entry: {
+        main:'./src/index.js',  //указываем входной файл
+        analytics: './src/analytics.js' //ещё один входной файл
+    },
     output: {                 //место для складвания
-        filename: 'bundle.js',
+        filename: '[name].bundle.js',  //для устранения ошибки указываем паттерн[name]
         path: path.resolve(__dirname, 'dist') //отталкиваясь от текущей директории (системная переменная __dirname) указываем пкть в папку dist
-    }
+    },
 }
